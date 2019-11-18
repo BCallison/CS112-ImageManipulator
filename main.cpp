d48d308d0bb9819c0e60f120b983b7b4bf5fa4ac
Blake Callison <blakebcallison@gmail.com>
2019-11-18 14:38:07 -0800

    Began swapping framework

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <sstream>
 #include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,7 +14,6 @@ int main(int argc, char* argv[])
 	string file;
 
 	ifstream inPix;
-	ofstream output_file;
 	inPix.open(file);
 	string input_file;
 	string line;
@@ -25,10 +25,6 @@ int main(int argc, char* argv[])
 	int color_val = 0;
 	int gray_val = 0;
 	int count = 0;
-	string line1;
-	string line2;
-	string line3;
-	string line4;
 	int randNum = 0;
 	int randMin = -10;
 	int randMax = 10;
@@ -94,35 +90,39 @@ int main(int argc, char* argv[])
 		image_dimensions >> num_columns;
 		image_dimensions >> num_rows;
 
-
-
-		while (data.size() < (output.size() / num_columns))
-		{
-			vector<int> temp;
-			for (int x = 0; x < output.size(); x++)
+		//resizing?
+		while (!input.eof()) {
+			for (int i = 0; i < 1; i++)
 			{
-				if (temp.size() < (num_rows * 3))
+				vector<string> tmpVec;
+				string tmpString;
+			
+				for (int j = 0; j < 4; j++) 
 				{
-					temp.push_back(output[x]);
-				}
+						myReadFile >> tmpString;
+						tmpVec.push_back(tmpString);
+					}
+				data.push_bac(tmpVec);
 			}
-			data.push_back(temp);
 		}
 
-		cout << output.size() << endl;
-		cout << data.size();
+		// Begin by setting number of rows
+		data.resize(num_rows * 3);
 
+		// Flip Horizontally
 		if (image_effect == '10')
 		{
-			for (int i = 0; i < data.size(); i++)
+			//resize each row to be correct width
+			for (int i = 0; i < num_columns; i++)
 			{
-				for (int x = 0, y = (data[i].size() - 1); x < data[i].size() && y >= 0; x++, y--)
+				for (int j = 0; j < (num_rows / 2); j++)
 				{
-					data[i][x] = data[i][y];
-					data[i][y] = data[i][x];
+					swap(data[i], data[num_columns - i]);
+					cout << data[i] << " " << data[num_columns - i];
 				}
 			}
 		}
+
 
 		/* vector<int> vals{};
 		for (int i = 3; i != lines.size(); i++)
@@ -367,23 +367,6 @@ int main(int argc, char* argv[])
 
 		}
 */
-
-output_file.open(output_file_name);
-
-output_file << line1 << endl
-<< line2 << endl
-<< line3 << endl
-<< line4 << endl;
-
-
-for (int i = 0; i < data.size(); i++)
-{
-	for (int x = 0; x < data[i].size(); x++)
-	{
-		output_file << data[i][x] << ' ';
-	}
-	output_file << endl;
-}
 
 
 		// Quits program
